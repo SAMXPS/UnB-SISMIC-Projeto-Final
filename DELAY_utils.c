120722eb3f7a617836e793f6aa1617b97ae242b5
Samuel James <bsbcraftplays@gmail.com>
2021-11-04 10:35:35 -0300

    Importando função para delay em micro-segundos

---
+++ DELAY_utils.c
@@ -0,0 +1,16 @@
+#include <msp430.h>
+
+/*
+ * Delay microsseconds.
+ */
+void delay_us(unsigned int time_us) {
+    //Configure timer A0 and starts it.
+    TA0CCR0 = time_us;
+    TA0CTL = TASSEL__SMCLK | ID__1 | MC_1 | TACLR;
+
+    //Locks, waiting for the timer.
+    while((TA0CTL & TAIFG) == 0);
+
+    //Stops the timer
+    TA0CTL = MC_0 | TACLR;
+}
---
+++ DELAY_utils.h
@@ -0,0 +1,8 @@
+#ifndef DELAY_UTILS_H_
+#define DELAY_UTILS_H_
+/*
+ * Delay microsseconds.
+ */
+void delay_us(unsigned int time_us);
+
+#endif /* DELAY_UTILS_H_ */
